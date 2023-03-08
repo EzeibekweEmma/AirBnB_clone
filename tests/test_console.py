6052ac55c039a3fef6856be2cc03e0a511e8c41d
thebill1000 <williamsbilljnr100@gmail.com>
2023-03-07 22:10:26 -0800

    Tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -504,3 +504,238 @@ class TestHBNBCommand(unittest.TestCase):
             s = f.getvalue()
             self.assertIn(attr, s)
             self.assertIn(val, s)
+
+        def test_update_6(self):
+            """Tests update 1..."""
+            classname = "Review"
+            attr = "foostr"
+            val = "fooval"
+            uid = self.create_class(classname)
+            cmd = '{}.update("{}", "{}", "{}")'
+            #  cmd = 'update {} {} {} {}'
+            cmd = cmd.format(classname, uid, attr, val)
+            #  print("CMD::", cmd)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd(cmd)
+            s = f.getvalue()
+            self.assertEqual(len(s), 0)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('{}.show("{}")'.format(classname, uid))
+            s = f.getvalue()
+            self.assertIn(attr, s)
+            self.assertIn(val, s)
+
+        def test_update_7(self):
+            """Tests update 1..."""
+            classname = "Place"
+            attr = "foostr"
+            val = "fooval"
+            uid = self.create_class(classname)
+            cmd = '{}.update("{}", "{}", "{}")'
+            #  cmd = 'update {} {} {} {}'
+            cmd = cmd.format(classname, uid, attr, val)
+            #  print("CMD::", cmd)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd(cmd)
+            s = f.getvalue()
+            self.assertEqual(len(s), 0)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('{}.show("{}")'.format(classname, uid))
+            s = f.getvalue()
+            self.assertIn(attr, s)
+            self.assertIn(val, s)
+
+        def test_update_everything(self):
+            """Tests update command with errthang, like a baws."""
+            for classname, cls in self.classes().items():
+                uid = self.create_class(classname)
+                for attr, value in self.test_random_attributes.items():
+                    if type(value) is not str:
+                        pass
+                    quotes = (type(value) == str)
+                    self.help_test_update(classname, uid, attr,
+                                          value, quotes, False)
+                    self.help_test_update(classname, uid, attr,
+                                          value, quotes, True)
+                pass
+                if classname == "BaseModel":
+                    continue
+                for attr, attr_type in self.attributes()[classname].items():
+                    if attr_type not in (str, int, float):
+                        continue
+                    self.help_test_update(classname, uid, attr,
+                                          self.attribute_values[attr_type],
+                                          True, False)
+                    self.help_test_update(classname, uid, attr,
+                                          self.attribute_values[attr_type],
+                                          False, True)
+
+        def help_test_update(self, classname, uid, attr, val, quotes, func):
+            """Tests update commmand."""
+            #  print("QUOTES", quotes)
+            FileStorage._FileStorage__objects = {}
+            if os.path.isfile("file.json"):
+                os.remove("file.json")
+            uid = self.create_class(classname)
+            value_str = ('"{}"' if quotes else '{}').format(val)
+            if func:
+                cmd = '{}.update("{}", "{}", {})'
+            else:
+                cmd = 'update {} {} {} {}'
+            cmd = cmd.format(classname, uid, attr, value_str)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd(cmd)
+            msg = f.getvalue()[:-1]
+            # print("MSG::", msg)
+            # print("CMD::", cmd)
+            self.assertEqual(len(msg), 0)
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('{}.show("{}")'.format(classname, uid))
+            s = f.getvalue()
+            self.assertIn(str(val), s)
+            self.assertIn(attr, s)
+
+        def test_do_update_error(self):
+            """Tests update command with errors."""
+            uid = self.create_class("BaseModel")
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** class name missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update garbage")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** class doesn't exist **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update BaseModel")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** instance id missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update BaseModel 6534276893")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** no instance found **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('update BaseModel {}'.format(uid))
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** attribute name missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('update BaseModel {} name'.format(uid))
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** value missing **")
+
+        def test_do_update_error_advanced(self):
+            """Tests update() command with errors."""
+            uid = self.create_class("BaseModel")
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd(".update()")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** class name missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("garbage.update()")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** class doesn't exist **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("BaseModel.update()")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** instance id missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("BaseModel.update(6534276893)")
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** no instance found **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('BaseModel.update("{}")'.format(uid))
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** attribute name missing **")
+
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('BaseModel.update("{}", "name")'.format(uid))
+            msg = f.getvalue()[:-1]
+            self.assertEqual(msg, "** value missing **")
+
+        def create_class(self, classname):
+            """Creates a class for console tests."""
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("create {}".format(classname))
+            uid = f.getvalue()[:-1]
+            self.assertTrue(len(uid) > 0)
+            return uid
+
+        def help_load_dict(self, rep):
+            """Helper method to test dictionary equality."""
+            rex = re.compile(r"^\[(.*)\] \((.*)\) (.*)$")
+            res = rex.match(rep)
+            self.assertIsNotNone(res)
+            s = res.group(3)
+            s = re.sub(r"(datetime\.datetime\([^)]*\))", "'\\1'", s)
+            d = json.loads(s.replace("'", '"'))
+            return d
+
+        def classes(self):
+            """Returns a dictionary of valid classes and their references."""
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.state import State
+            from models.city import City
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import
+
+            classes = {"BaseModel": BaseModel,
+                    "User": User,
+                    "State": State,
+                    "City": City,
+                    "Amenity": Amenity,
+                    "Place": Place,
+                    "Review": Review}
+            return classes
+
+        def attributes(self):
+            """Returns the valid attributes and their types for classname."""
+            attributes = {
+                    "BaseModel":
+                             {"id": str,
+                              "created_at": datetime.datetime,
+                              "updated_at": datetime.datetime},
+                    "User":
+                             {"email": str,
+                              "password": str,
+                              "first_name": str,
+                              "last_name": str},
+                    "State":
+                             {"name": str},
+                    "City":
+                             {"state_id": str,
+                              "name": str},
+                    "Amenity":
+                             {"name": str},
+                    "Place":
+                             {"city_id": str,
+                              "user_id": str,
+                              "name": str,
+                              "description": str,
+                              "number_rooms": int,
+                              "number_bathrooms": int,
+                              "max_guest": int,
+                              "price_by_night": int,
+                              "latitude": float,
+                              "longitude": float,
+                              "amenity_ids": list},
+                    "Review":
+                             {"place_id": str,
+                              "user_id": str,
+                              "text": str}
+                }
+                return attributes
+
+
+if __name__ == "__main__":
+    unittest.main()
