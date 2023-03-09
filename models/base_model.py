8bda897a9ab9ae61a8e5566c5532fb988cdeeff2
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-03-09 15:27:36 +0100

    class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""BaseModel for Airbnb Project"""
+import uuid
+from datetime import datetime
+import models
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel():
+    """Base class for classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, self.string2time(value))
+                    continue
+
+                setattr(self, key, value)
+
+        else:
+            uuid_gen = uuid.uuid4()
+            self.id = str(uuid_gen)
+            now = datetime.now()
+            self.created_at = now
+            self.updated_at = now
+
+            models.storage.new(self)
+
+    @staticmethod
+    def string2time(date_string):
+        """Converts string to time"""
+        return datetime.strptime(date_string, time)
+
+    def __str__(self):
+        """Returns string representation of an instance"""
+        return("[{}] ({}) {}"
+               .format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """Save an instance and set the updated time"""
+        self.updated_at = datetime.now()
+        models.storage.save()
